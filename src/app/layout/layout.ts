4c9e18b3540ab266d4fc441d0657fed586d76fab
Flash <zoom.flashych02@gmail.com>
2026-05-24 23:48:11 +0500

    Some fixes

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
+import { RouterOutlet } from "../../../node_modules/@angular/router/types/_router_module-chunk";
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, Footer],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
