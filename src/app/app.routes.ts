2f4913085be2c8a7f1276741e854d4837dd79a3e
Flash <zoom.flashych02@gmail.com>
2026-05-24 23:45:09 +0500

    Added header, footer and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,55 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  /*
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component')
+        .then(m => m.LoginComponent)
+  },
+
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/layout.component')
+        .then(m => m.LayoutComponent),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/home/home.component')
+            .then(m => m.HomeComponent)
+      },
+      {
+        path: 'catalog',
+        loadComponent: () =>
+          import('./pages/catalog/catalog.component')
+            .then(m => m.CatalogComponent)
+      },
+      {
+        path: 'new-arrivals',
+        loadComponent: () =>
+          import('./pages/new-arrivals/new-arrivals.component')
+            .then(m => m.NewArrivalsComponent)
+      },
+      {
+        path: 'deals',
+        loadComponent: () =>
+          import('./pages/deals/deals.component')
+            .then(m => m.DealsComponent)
+      },
+      {
+        path: 'cart',
+        loadComponent: () =>
+          import('./pages/cart/cart.component')
+            .then(m => m.CartComponent)
+      }
+    ]
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
+  }*/
+];
