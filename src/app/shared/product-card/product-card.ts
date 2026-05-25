1987a271d64eb9662f068c4b5d7d809881ac6b69
viktoria <kingsviktory@gmail.com>
2026-05-26 00:12:24 +0300

    feat: add favorites service, add to favorite btn in card component

--- src/app/shared/product-card/product-card.ts
+++ src/app/shared/product-card/product-card.ts
@@ -1,13 +1,23 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, computed } from '@angular/core';
+import { FavoritesService } from '../services/favourites.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './product-card.html',
 })
 export class ProductCard {
+  private favorites = inject(FavoritesService);
+
   readonly title = input.required<string>();
   readonly rating = input.required<number>();
   readonly price = input.required<number>();
   readonly imgSrc = input<string>('');
+
+  readonly isFavorite = computed(() => this.favorites.checkIsFavorite(this.title()));
+
+  toggleItem() {
+    this.favorites.toggleFavorite(this.title());
+  }
 }
