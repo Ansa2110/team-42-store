f7db8da0ba9b70ea0eec3637bbad7daab7ca0ae4
viktoria <kingsviktory@gmail.com>
2026-05-29 00:29:33 +0300

    feat: add catalog page; add filters; fix layout

---
+++ src/app/catalog-page/catalog-filters/catalog-filters.ts
@@ -0,0 +1,37 @@
+import { Component, input, output } from '@angular/core';
+import { CatalogFiltersType } from '../catalog-page.types';
+import { MatDivider } from '@angular/material/divider';
+import { MatList } from '@angular/material/list';
+import { MatSlider, MatSliderModule } from '@angular/material/slider';
+
+@Component({
+  selector: 'app-catalog-filters',
+  imports: [MatDivider, MatList, MatSlider, MatSliderModule],
+  templateUrl: './catalog-filters.html',
+  styleUrl: './catalog-filters.css',
+})
+export class CatalogFilters {
+  readonly currentFilters = input.required<CatalogFiltersType>();
+
+  readonly filtersChange = output<Partial<CatalogFiltersType>>();
+
+  readonly categories = [{ title: 'aaa' }, { title: 'bbbb' }];
+
+  selectCategory(categoryTitle: string) {
+    this.filtersChange.emit({ category: categoryTitle });
+  }
+
+  onSearchInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.filtersChange.emit({ search: inputElement.value });
+  }
+
+  onPriceRangeChange(event: Event, value: keyof CatalogFiltersType) {
+    const inputElement = event.target as HTMLInputElement;
+    this.filtersChange.emit({ [value]: inputElement.value });
+  }
+
+  set<K extends keyof CatalogFiltersType>(key: K, value: CatalogFiltersType[K]) {
+    this.filtersChange.emit({ [key]: value });
+  }
+}
