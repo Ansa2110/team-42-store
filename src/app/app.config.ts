58ed652f3be03850715b600eff5db719bb97ed6c
Angelina Vakkassova <angelina.vksv89@gmail.com>
2026-06-22 14:08:43 +0500

    feat: enhance auth forms and integrate backend and OAuth authentication flow

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { environment } from '../environments/environment.local';
 import { routes } from './app.routes';
+import { authInterceptor } from './auth/auth.interceptor';
+import { API_URL, AUTH_TOKEN_STORAGE_KEY } from './auth/auth.tokens';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+
+    { provide: API_URL, useValue: environment.apiUrl },
+    { provide: AUTH_TOKEN_STORAGE_KEY, useValue: 'shopfront_auth_token' },
+  ],
 };
